6a99461aefb9f87316bdb7e63b633df41fe2febf
Erick Fernando <erickfernandoxavier@hotmail.com>
2023-08-30 14:50:12 -0300

    Update slicepathsurl.go

--- slicepathsurl.go
+++ slicepathsurl.go
@@ -16,7 +16,6 @@ func containsPercent(word string) bool {
 			return true
 		}
 	}
-
 	return false
 }
 
@@ -30,7 +29,6 @@ func containsSpecialChars(word string, chars string) bool {
 }
 
 func main() {
-
 	levelPtr := flag.Int("l", 2, "Level [1, 2 or 3]")
 	flag.Parse()
 
@@ -40,11 +38,13 @@ func main() {
 	var urlsList []string
 
 	for scanner.Scan() {
-
 		urlStr := scanner.Text()
 		if containsPercent(urlStr) {
 			continue
 		}
+
+		// Remove query parameters from URL
+		urlStr = strings.Split(urlStr, "?")[0]
 
 		urlParts, err := url.Parse(urlStr)
 		if err != nil {
@@ -64,31 +64,56 @@ func main() {
 
 				path := strings.Join(pathParts[:i+1], "/")
 				counter := strings.Count(path, "-")
-				chars := ":*;(){}[]@\".&"
+				chars := ":*;(){}[]@\"&'+"
 
 				if containsSpecialChars(path, chars) || counter > 1 {
 					continue
-				} else {
+				} else if strings.Contains(path, ".") {
+					suffixes := []string{".php", ".aspx", ".jsf", ".asp", ".html", ".jsonp", ".json", ".jsp"}
 
+					// Check if the path has any of the valid suffixes
+					hasValidSuffix := false
+					for _, suffix := range suffixes {
+						if strings.HasSuffix(path, suffix) {
+							hasValidSuffix = true
+							break
+						}
+					}
+
+					// If a valid suffix was found, include the URL
+					if hasValidSuffix {
+						resultantUrl := urlParts.Scheme + "://" + urlParts.Host + "/" + path
+						index := strings.Index(resultantUrl, "//")
+
+						if index != -1 {
+							secondIndex := strings.Index(resultantUrl[index+2:], "//")
+							if secondIndex != -1 {
+								resultantUrl = resultantUrl[:index+2+secondIndex] + "/" + resultantUrl[index+2+secondIndex+2:]
+							}
+						}
+
+						urlsList = append(urlsList, resultantUrl)
+					}
+				} else {
 					values := strings.Split(path, "/")
-					verify_numeric_values := 0
+					verifyNumericValues := 0
 
 					for _, value := range values {
 						if _, err := strconv.Atoi(value); err == nil {
-							verify_numeric_values = verify_numeric_values + 1
+							verifyNumericValues = verifyNumericValues + 1
 						} else {
 							continue
 						}
 					}
-					if verify_numeric_values > 0 {
+					if verifyNumericValues > 0 {
 						continue
 					} else {
 						resultantUrl := urlParts.Scheme + "://" + urlParts.Host + "/" + path
-						index := strings.Index(resultantUrl, "//") 
+						index := strings.Index(resultantUrl, "//")
 
 						if index != -1 {
 							secondIndex := strings.Index(resultantUrl[index+2:], "//")
-							if secondIndex != -1 { 
+							if secondIndex != -1 {
 								resultantUrl = resultantUrl[:index+2+secondIndex] + "/" + resultantUrl[index+2+secondIndex+2:]
 							}
 						}
@@ -100,7 +125,7 @@ func main() {
 		}
 	}
 
-	// Removes URls duplicadas da lista
+	// Removes duplicated URLs from the list
 	urlsMap := make(map[string]bool)
 	for _, url := range urlsList {
 		urlsMap[url] = true
@@ -111,7 +136,7 @@ func main() {
 		resultantUrlsList = append(resultantUrlsList, url)
 	}
 
-	// Printa o resultado da lista
+	// Print the resulting list
 	for _, url := range resultantUrlsList {
 		fmt.Println(url)
 	}
