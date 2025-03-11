bd973aedc0f41ce6582925419f736c93f0f9ae7a
Erick Fernando <erickfernandoxavier@hotmail.com>
2025-03-11 15:41:35 -0300

    Update slicepathsurl.go

--- slicepathsurl.go
+++ slicepathsurl.go
@@ -69,7 +69,7 @@ func main() {
 				if containsSpecialChars(path, chars) || counter > 1 {
 					continue
 				} else if strings.Contains(path, ".") {
-					suffixes := []string{".php", ".php3", ".php4", ".aspx", ".jsf", ".asp", ".html", ".jsonp", ".json", ".jsp", ".axd", ".htm", ".esp", ".cgi", ".do", ".jsx", ".xhtml"}
+					suffixes := []string{".php", ".php3", ".php4", ".aspx", ".jsf", ".asp", ".html", ".jsonp", ".json", ".jsp", ".axd", ".htm", ".esp", ".cgi", ".do", ".jsx", ".xhtml", ".jhtm"}
 
 					// Check if the path has any of the valid suffixes
 					hasValidSuffix := false
